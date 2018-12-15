81ba4bbb26c7651fbbb62184cbacc1ed3e5961ea
cody <maxiecody@gmail.com>
2018-12-14 21:18:33 -0600

    added bouncing block and class based styling

--- basic_animations.js
+++ basic_animations.js
@@ -18,7 +18,7 @@ const resizingBlocks = () => {
 	let horizontalBlock = document.getElementById('resizing-element-horizontal');
 	let verticalBlock = document.getElementById('resizing-element-vertical');
 	let currentSize = 100;
-	let animation = setInterval(function() {resizeBlocks()}, 10);
+	let animation = setInterval(function() {resizeBlocks()}, 20);
 	let direction = 1;
 	const resizeBlocks = () => {
 		if (horizontalBlock.style.width == '200px') {
@@ -27,9 +27,39 @@ const resizingBlocks = () => {
 			direction = 1;
 		}
 		currentSize += direction;
-		horizontalBlock.style.marginRight = `${205-currentSize}px`
+		horizontalBlock.style.marginRight = `${220-currentSize}px`
+
 		horizontalBlock.style.width = currentSize + 'px';
 		verticalBlock.style.height = currentSize + 'px';
-	}
+	};
+};
 
-}
+const bounceBlock = () => {
+	let bouncingBlock = document.getElementById('bouncing-block');
+	let bounceInterval = setInterval(function() {bounce()}, .1);
+	let lastBounce = 0;
+	let pixelPos = 0;
+	let movingUp = true;
+	const bounce = () => {
+		if (lastBounce == 0) {
+			lastBounce = 140;
+		} else if (lastBounce < 1) {
+			lastBounce = 0;
+			pixelPos = 0;
+			movingUp = true;
+		}
+		
+		if (movingUp == true && pixelPos != lastBounce) {
+			pixelPos++;
+			bouncingBlock.style.bottom = `${pixelPos}px`;
+		} else if (movingUp == true && pixelPos == lastBounce) {
+			movingUp = false;
+		} else if (movingUp == false && pixelPos != 0) {
+			pixelPos--;
+			bouncingBlock.style.bottom = `${pixelPos}px`;
+		} else if (movingUp == false && pixelPos == 0) {
+			movingUp = true;
+			lastBounce = Math.floor(lastBounce/1.5);
+		}
+	}
+};
